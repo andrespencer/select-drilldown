780a8751304f6b126aaa6548247eace3e4d662bf
André Spencer <andrespencer@pm.me>
2019-12-12 22:14:32 +0100

    Convert App.js to ES6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.scss';
+import React, {PureComponent} from 'react'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="Header">
-        <img className ="Header__Logo" src={process.env.PUBLIC_URL + "/img/logo.png"} />
-      </header>
-    </div>
-  );
+class App extends PureComponent {
+  render () {
+    return (
+      <div className="App">
+        <header className="Header">
+          <img className ="Header__Logo" src={process.env.PUBLIC_URL + "/img/logo.png"} />
+        </header>
+      </div>
+    )
+  } 
 }
 
-export default App;
+export default App
